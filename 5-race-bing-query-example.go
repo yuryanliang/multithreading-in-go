29601940a3d68db4741a3f75d48acc90d543bc4e
Cory Finger <cory.j.finger@gmail.com>
2018-02-18 18:58:28 -0800

    Start using buffered channels faster to avoid leaking goroutines

--- 5-race-bing-query-example.go
+++ 5-race-bing-query-example.go
@@ -10,7 +10,7 @@ func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	query := "Our Query"
-	respond := make(chan string)
+	respond := make(chan string, 2)
 
 	go googleIt(respond, query)
 	go bingIt(respond, query)
